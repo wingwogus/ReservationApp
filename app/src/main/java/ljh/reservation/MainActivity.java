79fdc4a0e3be916152dfd150ffe1924852311673
leejaehyun <wingwogus@naver.com>
2024-11-15 13:47:45 +0900

    add explain

--- app/src/main/java/ljh/reservation/MainActivity.java
+++ app/src/main/java/ljh/reservation/MainActivity.java
@@ -54,8 +54,10 @@ public class MainActivity extends AppCompatActivity {
         poster2 = findViewById(R.id.poster2);
         poster3 = findViewById(R.id.poster3);
 
-        movieDay = LocalDate.now().format(DateTimeFormatter.ofPattern("MM월 dd일"));
+        //상영 날짜 지정
+        movieDay = "11월23일";
 
+        //영화 목록 배열
         String[] movies = {movieDay + ": 베놈", movieDay + ": 청설", movieDay + ": 비긴어게인"};
 
         ArrayAdapter<String> movieAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, movies);
@@ -63,19 +65,21 @@ public class MainActivity extends AppCompatActivity {
 
         result.setTextColor(Color.GREEN);
 
+        //포스트 보기 버튼 클릭 시 예약 체크 메소드
         btnShow.setOnClickListener(e -> {
             if (checkReservation(actv.getText().toString())) {
                 showPoster();
             } else {
                 divRes.setVisibility(View.INVISIBLE);
             }
-            System.out.println("로그인 기능");
         });
 
+        //이전 및 다음 버튼 리스너 설정
         btnPrev.setOnClickListener(e -> vf.showPrevious());
 
         btnNext.setOnClickListener(e -> vf.showNext());
 
+        //타임피커 시간 병동 시 LocalTime과  DateTimeFormatter를 사용해 시와 분을 항상 2자리로 출력
         tp.setOnTimeChangedListener((view, hourOfDay, minute) -> {
             hour = hourOfDay;
             min = minute;
@@ -86,12 +90,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //예약 체크 메소드
     public boolean checkReservation(String s) {
+        //텍스트를 :으로 구별하여 토큰으로 나눔
         StringTokenizer tk = new StringTokenizer(s, ":");
         try {
+            //앞 토큰은 날짜, 뒤 토큰은 영화 이름
             String inputMovieDay = tk.nextToken();
             movieName = tk.nextToken().trim();
 
+            //만약 날짜가 지정한 날짜와 같지 않다면 메세지 출력 및 false 리턴
+            //만약 영화 이름이 영화 목록에 없다면 메세지 출력 및 false 리턴
             if (!inputMovieDay.equals(movieDay)) {
                 Toast.makeText(this, "해당 날짜에는 영화를 상영하지 않습니다", Toast.LENGTH_SHORT).show();
                 return false;
@@ -99,13 +108,17 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "올바른 영화 이름을 적어주세요", Toast.LENGTH_SHORT).show();
                 return false;
             }
+
+            //모든 게 정상이면 true 리턴
             return true;
         } catch (NoSuchElementException e) {
+            //토큰이 나눠지지 않았을 때 메세지 출력
             Toast.makeText(this, "날짜와 공연을 콜론(:)으로 구별하여 입력해주세요", Toast.LENGTH_SHORT).show();
             return false;
         }
     }
 
+    //선택한 영화에 맞는 포스터를 보여주는 메소드
     public void showPoster() {
         divRes.setVisibility(View.VISIBLE);
         switch (movieName) {
